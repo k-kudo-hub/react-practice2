fa7337dbec8270e5ed3980ca10cf5e045d5ba73d
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-31 18:12:03 +0900

    Review useState.

--- section_1/src/App.js
+++ section_1/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [countNum, setCountNum] = useState(0);
+  const countUp = () => setCountNum(countNum + 1);
+  const countDown = () => setCountNum(countNum - 1);
   return (
     <div>
       <h1>Hello, World!</h1>
+      <div style={{ display: 'flex' }}>
+        <button onClick={countUp}>CountUp</button>
+        <button onClick={countDown}>CountDown</button>
+      </div>
+      <p>{countNum}</p>
     </div>
   )
 }
