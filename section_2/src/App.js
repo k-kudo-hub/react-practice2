f2ef6d563c532fbb9c0afdd65ffd7c4595287327
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-02 21:54:06 +0900

    Learn how to apply style(Styled components).

--- section_2/src/App.js
+++ section_2/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
 import InlineStyle from './components/InlineStyle'
 import CssModules from './components/CssModules'
+import StyledJsx from './components/StyledJsx'
+import "./App.css"
 
 export default function App() {
   return (
     <div>
       <h1>Hello, world</h1>
-      <InlineStyle/>
-      <CssModules/>
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
     </div>
   );
 }
