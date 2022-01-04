4b5c6f7a77e2a09b9b95fcadc24f812084e80197
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-04 22:01:24 +0900

    Minior fix.

--- section_3/src/components/Home.jsx
+++ section_3/src/components/Home.jsx
@@ -1,15 +1,7 @@
-import { Link } from "react-router-dom";
-import styles from "../styles/Home.module.scss";
-
 const Home = () => {
-  const { links } = styles;
   return (
     <div>
       <h1>Home is here!</h1>
-      <div className={links}>
-        <Link to="/about">About Page</Link>
-        <Link to="/contact">Contact Page</Link>
-      </div>
     </div>
   )
 }
