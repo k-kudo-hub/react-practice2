98015a734fe329d239f8b6e301c8599d70791793
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-04 21:56:39 +0900

    Created some child components.

---
+++ section_3/src/components/AboutContentA.jsx
@@ -0,0 +1,10 @@
+const AboutContent_1 = () => {
+  return (
+    <div>
+      <h2>About Content A</h2>
+      <p>Lorem...Lorem...Lorem...Lorem...Lorem...</p>
+    </div>
+  )
+}
+
+export default AboutContent_1;
---
+++ section_3/src/components/AboutContentB.jsx
@@ -0,0 +1,10 @@
+const AboutContent_2 = () => {
+  return (
+    <div>
+      <h2>About Content B</h2>
+      <p>Lorem...Lorem...Lorem...Lorem...Lorem...</p>
+    </div>
+  )
+}
+
+export default AboutContent_2;
