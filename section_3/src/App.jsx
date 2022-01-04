1faa74ac4d80fb86f5cf636bc6636c13f044f517
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-04 17:11:06 +0900

    Set up the routing using the React dom router.

--- section_3/src/App.jsx
+++ section_3/src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 
 const App = () => {
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
