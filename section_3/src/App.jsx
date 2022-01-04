abab8b66fe4b99ed9174c054f5111254a38e0e3d
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-04 17:08:27 +0900

    Initialize some components.

--- section_3/src/App.jsx
+++ section_3/src/App.jsx
@@ -1,1 +1,12 @@
+import "./App.css";
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+  )
+}
+
+export default App;
 
