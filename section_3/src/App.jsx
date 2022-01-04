9c79f60a8873ae9eedf182c16edee36e11f7d273
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-04 21:59:40 +0900

    Set the react routing.

--- section_3/src/App.jsx
+++ section_3/src/App.jsx
@@ -2,15 +2,19 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
+import AboutContentA from "./components/AboutContentA";
+import AboutContentB from "./components/AboutContentB";
 import Contact from './components/Contact';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="about" element={<About />}></Route>
-        <Route path="contact" element={<Contact />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="about/contentA" element={<AboutContentA />} />
+        <Route path="about/contentB" element={<AboutContentB />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   )
