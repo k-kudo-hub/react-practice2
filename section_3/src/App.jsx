f63b8a3f9747f5ec54535aa17076490d25a1f5d1
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-04 22:01:09 +0900

    Use the link component to set up the link.

--- section_3/src/App.jsx
+++ section_3/src/App.jsx
@@ -5,8 +5,10 @@ import About from './components/About';
 import AboutContentA from "./components/AboutContentA";
 import AboutContentB from "./components/AboutContentB";
 import Contact from './components/Contact';
+import styles from './styles/Home.module.scss';
 
 const App = () => {
+  const { links } = styles;
   return (
     <BrowserRouter>
       <Routes>
@@ -16,9 +18,14 @@ const App = () => {
         <Route path="about/contentB" element={<AboutContentB />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
+      
+      <nav className={links}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
     </BrowserRouter>
   )
 }
 
 export default App;
-
--- section_3/src/components/About.jsx
+++ section_3/src/components/About.jsx
@@ -1,7 +1,16 @@
+import { Link, Outlet } from "react-router-dom";
+import styles from "../styles/Home.module.scss";
+
 const About = () => {
+  const { links } = styles;
   return (
     <div>
       <h1>About is here!</h1>
+      <div className={links}>
+        <Link to="contentA">About Content A</Link>
+        <Link to="contentB">About Content B</Link>
+      </div>
+      <Outlet/>
     </div>
   )
 }
