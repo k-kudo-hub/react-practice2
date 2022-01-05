283aaab3b0a64bc454c1ef95d27a881c8b39b0d1
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-05 16:13:18 +0900

    Page transitions were implemented using useNavigate.

--- section_3/src/components/About.jsx
+++ section_3/src/components/About.jsx
@@ -1,17 +1,28 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import styles from "../styles/Home.module.scss";
 
 const About = () => {
   const { links } = styles;
   const arr = [...Array(100).keys()];
 
+  // v5
+  // const history = useHistory();
+  // const onClickContentA = () => history.push("/about/contentA");
+
+  // v6
+  const navigate = useNavigate();
+  const onClickContentA = () => navigate("/about/contentA");
+
   return (
     <div>
       <h1>About is here!</h1>
       <div className={links}>
         {/* v5 => <Link to={{ to="contentA", state={arr} }}> */}
-        <Link to={"contentA"} state={arr}>About Content A</Link>
+        <Link to="contentA" state={arr}>About Content A</Link>
         <Link to="contentB">About Content B</Link>
+      </div>
+      <div className={links}>
+        <button onClick={onClickContentA}>DetailA</button>
       </div>
       <Outlet/>
     </div>
