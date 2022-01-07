a05dd7fa84b0ed9ccc8d3d42441e1060f6748d06
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-07 16:51:49 +0900

    User profile cards were created as organisms.

---
+++ section_4/src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
+
+export const UserCard = (props) => {
+  const { user } = props;
+  return (
+    <Card>
+      <UserIconWithName
+        image={user.image}
+        name={user.name}
+      />
+      <SDl>
+        <dt>mail</dt>
+        <dd>{user.email}</dd>
+        <dt>tel</dt>
+        <dd>{user.phone}</dd>
+        <dt>company</dt>
+        <dd>{user.company.name}</dd>
+        <dt>web</dt>
+        <dd>{user.web}</dd>
+      </SDl>
+    </Card>
+  )
+}
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+`
