bdad18f2b65c06596e65a45b0c16bd816f53245f
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-08 14:53:09 +0900

    Created a router and cut it out into a folder for the router.

--- section_4/src/App.jsx
+++ section_4/src/App.jsx
@@ -1,10 +1,4 @@
-import PrimaryButton from "./components/atoms/button/PrimaryButton";
-import SecondaryButton from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { BrowserRouter } from "react-router-dom";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./Route/Router";
 
 const user = {
   name: "Bond",
@@ -19,15 +13,7 @@ const user = {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <h1>Hello, Atomic Design.</h1>
-        <PrimaryButton>test</PrimaryButton>
-        <SecondaryButton>search</SecondaryButton>
-        <SearchInput/>
-        <UserCard user={user}/>
-      </DefaultLayout>
-    </BrowserRouter>
+    <Router />
   )
 }
 
