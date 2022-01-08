bec1ca221d2a83b0ef4232b9daf6c41e189ef60e
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-08 15:11:19 +0900

    Transferred the user list display to the user page component.

--- section_4/src/App.jsx
+++ section_4/src/App.jsx
@@ -1,16 +1,5 @@
 import { Router } from "./Route/Router";
 
-const user = {
-  name: "Bond",
-  image: "https://source.unsplash.com/mJaD10XeD7w",
-  email: "aaa@example.com",
-  phone: "XXX-XXXX-XXXX",
-  company: {
-    name: "Test.inc"
-  },
-  web: "http://localhost.com"
-}
-
 const App = () => {
   return (
     <Router />
--- section_4/src/components/pages/Users.jsx
+++ section_4/src/components/pages/Users.jsx
@@ -1,13 +1,46 @@
 import styled from "styled-components"
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map(((val) => {
+  return {
+    id: val,
+    name: `Bond${val}`,
+    image: "https://source.unsplash.com/mJaD10XeD7w",
+    email: "aaa@example.com",
+    phone: "XXX-XXXX-XXXX",
+    company: {
+      name: "Test.inc"
+    },
+    web: "http://localhost.com"
+  }
+}))
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>Users PAGE</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   )
 }
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px 0;
+`
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `
