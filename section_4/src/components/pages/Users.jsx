ab9d66728c2ffbaacecfc7625f8b5684ab663cad
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-08 14:52:32 +0900

    Created a component for the page.

---
+++ section_4/src/components/pages/Users.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>Users PAGE</h2>
+    </SContainer>
+  )
+}
+
+const SContainer = styled.div`
+  text-align: center;
+`
