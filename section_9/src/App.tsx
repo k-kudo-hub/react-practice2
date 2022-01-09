ccb8772cba4330aeeb55bab45bc6ddfedfff97d7
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-09 17:58:20 +0900

    Practice - Specify the type of value to be returned -

--- section_9/src/App.tsx
+++ section_9/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Practice1 } from "./practices/Practice1";
+import { Practice2 } from "./practices/Practice2";
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
    <div>
      <h1>Hello, React * Typescript</h1>
      <Practice1/>
+     <Practice2/>
    </div>
   );
 }
