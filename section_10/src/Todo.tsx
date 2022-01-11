402917e9c269066fed48c69e99382196059fcfc9
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-11 12:57:46 +0900

    Learned how to use Pick to extract and use only the items you need.

--- section_10/src/Todo.tsx
+++ section_10/src/Todo.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
+import { TodoType } from "./types/todo";
 
-type TodoType = {
-  title: string;
-  userId: number;
-  completed?: boolean;
-}
-
-export const Todo = (props: TodoType) => {
+export const Todo = (
+  // "id" is not needed. So, use "Pick" to select other items from the TodoType. 
+  props: Pick<TodoType, "userId" | "title" | "completed">
+) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[x]" : "[ ]";
   return <p>{`${completeMark} ${title}(USER ID: ${userId})`}</p>
