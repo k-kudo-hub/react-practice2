49b825e8976046ac9ebc6ea51a1142881d8fa4e8
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-12 11:07:31 +0900

    Implemented data acquisition, formatting, and display.

--- section_11/src/App.tsx
+++ section_11/src/App.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { UserCard } from "./components/UserCard";
+import { User } from './types/api/user';
+import { UserProfile } from './types/UserProfile';
+
 export default function App (){
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfiles(data);
+      })
+      .catch((err) => console.log(err))
+  }
   return (
     <div>
       <h1>Hello, custom hook.</h1>
+      <button onClick={onClickFetchUser}>Get data</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user}/>
+      ))}
     </div>
   )
 }
