b310c8310105b5ee5b98ecefcb3f60ec5d08ff90
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-12 11:15:27 +0900

    Implemented display switching when loading and when an error occurs.

--- section_11/src/App.tsx
+++ section_11/src/App.tsx
@@ -6,7 +6,12 @@ import { UserProfile } from './types/UserProfile';
 
 export default function App (){
   const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
     axios
       .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
@@ -18,15 +23,26 @@ export default function App (){
         }));
         setUserProfiles(data);
       })
-      .catch((err) => console.log(err))
+      .catch((err) => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      })
   }
   return (
     <div>
       <h1>Hello, custom hook.</h1>
       <button onClick={onClickFetchUser}>Get data</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user}/>
-      ))}
+      {error ? (
+        <p style={{ color: "red" }}>Data loading error.</p>
+      ) : loading ? (
+        <p>Now loading...</p>
+      ) : (
+        userProfiles.map((user) => (
+          <UserCard key={user.id} user={user}/>
+        ))
+      )}
     </div>
   )
 }
