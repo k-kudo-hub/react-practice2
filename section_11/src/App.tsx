2528e00f175f8f08e7996180780d77eb7d256c5e
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-12 12:14:01 +0900

    The data retrieval and formatting was cut out as a custom hook.

--- section_11/src/App.tsx
+++ section_11/src/App.tsx
@@ -1,35 +1,11 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { UserCard } from "./components/UserCard";
-import { User } from './types/api/user';
-import { UserProfile } from './types/UserProfile';
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App (){
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
 
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }));
-        setUserProfiles(data);
-      })
-      .catch((err) => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      })
-  }
   return (
     <div>
       <h1>Hello, custom hook.</h1>
