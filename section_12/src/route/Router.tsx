a3917273db4acb8ebaa831874a67aa48380d8190
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-13 11:12:12 +0900

    Created a page structure without a footer.

--- section_12/src/route/Router.tsx
+++ section_12/src/route/Router.tsx
@@ -5,11 +5,12 @@ import { Login } from "../components/pages/Login";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router: VFC = memo(() => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<HeaderLayout><Home/></HeaderLayout>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/setting" element={<Setting/>}/>
       <Route path="/user_management" element={<UserManagement/>}/>
