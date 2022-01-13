9e64c532c44ebe2b9e94fa3e9c1d28eb6f62f529
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-13 10:17:38 +0900

    Used react-router-dom (Version 6) to set up the routing.

--- section_12/src/App.tsx
+++ section_12/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { ChakraProvider, Button } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import theme from './theme/theme';
+import { Router } from './route/Router';
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">Button</Button>
+      <BrowserRouter>
+        <Router/>
+        <Button colorScheme="teal">Button</Button>
+      </BrowserRouter>
     </ChakraProvider>
   )
 }
