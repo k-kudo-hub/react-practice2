7f341f5e60bc6204eff65861c17f0b5fa2edbdf0
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-13 16:04:14 +0900

    Developed the UI for the login page.

--- section_12/src/components/pages/Login.tsx
+++ section_12/src/components/pages/Login.tsx
@@ -1,9 +1,17 @@
 import React, { VFC, memo } from "react";
+import { Flex, Box, Button, Heading, Divider, Input, Stack } from "@chakra-ui/react";
 
 export const Login: VFC = memo(() => {
   return (
-    <div>
-      <h1>Login page is here.</h1>
-    </div>
-  )
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">User Management App</Heading>
+        <Divider my={4} />
+        <Stack spacing={6}>
+          <Input placeholder="User ID"/>
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>Login</Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
