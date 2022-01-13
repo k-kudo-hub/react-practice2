1ed99e9d027366fb5bf689fb14cead80a27f5a65
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-13 10:16:16 +0900

    Created a template for the necessary pages.

---
+++ section_12/src/components/pages/Login.tsx
@@ -0,0 +1,9 @@
+import React, { VFC, memo } from "react";
+
+export const Login: VFC = memo(() => {
+  return (
+    <div>
+      <h1>Login page is here.</h1>
+    </div>
+  )
+});
---
+++ section_12/src/components/pages/UserManagement.tsx
@@ -0,0 +1,9 @@
+import React, { VFC, memo } from "react";
+
+export const UserManagement: VFC = memo(() => {
+  return (
+    <div>
+      <h1>UserManagement page is here.</h1>
+    </div>
+  )
+});
