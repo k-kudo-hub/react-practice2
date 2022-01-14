ff55aef6a86539314459891140170e68340056eb
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-15 08:54:58 +0900

    Created the UI for the user card.

--- section_12/src/components/pages/UserManagement.tsx
+++ section_12/src/components/pages/UserManagement.tsx
@@ -1,9 +1,18 @@
 import React, { VFC, memo } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from '../organisms/user/UserCard';
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <div>
-      <h1>UserManagement page is here.</h1>
-    </div>
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          userName="Bond"
+          fullName="James Bond"
+          imageUrl="https://source.unsplash.com/random/"
+        />
+      </WrapItem>
+    </Wrap>
   )
 });
