c19316e2b63dbe5810b3daf0e9c145d4f3bcb096
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-14 13:20:07 +0900

    Developed actions for authentication, loading, and prohibiting input.

--- section_12/src/components/atoms/button/PrimaryButton.tsx
+++ section_12/src/components/atoms/button/PrimaryButton.tsx
@@ -3,12 +3,22 @@ import { Button } from '@chakra-ui/react';
 
 type Props = {
   children: ReactNode;
+  disabled?: boolean;
+  loading?: boolean;
+  onClick: () => void;
 }
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-  const { children } = props;
+  const { children, disabled = false, loading = false, onClick } = props;
   return (
-    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+    <Button
+      onClick={onClick}
+      bg="teal.400"
+      color="white"
+      isLoading={loading}
+      disabled={disabled || loading}
+      _hover={{ opacity: 0.8 }}
+    >
       { children }
     </Button>
   )
--- section_12/src/components/pages/Login.tsx
+++ section_12/src/components/pages/Login.tsx
@@ -1,17 +1,24 @@
-import React, { VFC, memo } from "react";
+import React, { VFC, memo, useState, ChangeEvent } from "react";
 import { Flex, Box, Heading, Divider, Input, Stack } from "@chakra-ui/react";
 
 import { PrimaryButton } from '../atoms/button/PrimaryButton';
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login: VFC = memo(() => {
+  const { Login, loading } = useAuth();
+  const [userId, setUserId] = useState('');
+  const onClickLogin = () => Login(userId);
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => setUserId(e.target.value);
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
         <Heading as="h1" size="lg" textAlign="center">User Management App</Heading>
         <Divider my={4} />
         <Stack spacing={6}>
-          <Input placeholder="User ID"/>
-          <PrimaryButton>Login</PrimaryButton>
+          <Input placeholder="User ID" value={userId} onChange={onChangeUserId}/>
+          <PrimaryButton disabled={userId === ""} onClick={onClickLogin} loading={loading}>
+            Login
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
