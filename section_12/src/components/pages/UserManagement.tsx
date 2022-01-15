ebf4b961f2120d1e488510627dfec93f248fee5d
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-15 10:06:53 +0900

    Using the hooks we created, we can now retrieve user data.

--- section_12/src/components/pages/UserManagement.tsx
+++ section_12/src/components/pages/UserManagement.tsx
@@ -1,18 +1,33 @@
-import React, { VFC, memo } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import React, { VFC, memo, useEffect } from "react";
+import { Wrap, WrapItem, Center, Spinner } from "@chakra-ui/react";
 
 import { UserCard } from '../organisms/user/UserCard';
+import { useAllUsers } from '../../hooks/useAllUsers';
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), [])
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          userName="Bond"
-          fullName="James Bond"
-          imageUrl="https://source.unsplash.com/random/"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner/>
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                userName={user.username}
+                fullName={user.name}
+                imageUrl="https://source.unsplash.com/random/"
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   )
 });
