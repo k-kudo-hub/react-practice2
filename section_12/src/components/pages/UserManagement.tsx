8ae1ccfc03b0ed361d7d65d76338544a8dd91158
k-kudo-hub <tongariisaibou@gmail.com>
2022-01-15 13:07:00 +0900

    Added the ability to keep logged-in user information in context.

--- section_12/src/components/pages/UserManagement.tsx
+++ section_12/src/components/pages/UserManagement.tsx
@@ -5,11 +5,14 @@ import { UserCard } from '../organisms/user/UserCard';
 import { UserDetailModal } from '../organisms/user/UserDetailModal';
 import { useAllUsers } from '../../hooks/useAllUsers';
 import { useSelectUser } from '../../hooks/useSelectUser';
+import { useLoginUser } from '../../hooks/useLoginUser';
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onClose, onOpen } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
+  console.log(loginUser);
 
   useEffect(() => getUsers(), [getUsers]);
 
